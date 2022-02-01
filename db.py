1d1daf522ea5e1fb99dcc5bcc370369444b17709
Alex van Winkel <alexicoo@gmail.com>
2022-02-01 09:56:51 -0500

    added static folder

--- db.py
+++ db.py
@@ -1,3 +1,40 @@
 import sqlite3
 connection = sqlite3.connect('database/points.db', check_same_thread=False)
 sql = connection.cursor()
+
+def create_tables():
+
+    query = '''
+
+    Create table if not exists users(
+        user_id integer primary key autoincrement,
+        name text,
+        username text,
+        password text,
+        email text,
+        avatar text
+    );
+
+
+    '''
+
+    sql.execute(query)
+
+
+    query = '''
+
+    Create table if not exists chores(
+        Speak tamil for a day text,
+        Fold clothes,
+        Fold clothes,
+        Fold bed,
+        Chess
+
+
+    );
+
+
+    '''
+
+    #sql.execute(query)
+    
--- users.py
+++ users.py
@@ -1,44 +1,8 @@
 from db import connection, sql
 
-def create_tables():
 
-    query = '''
-
-    Create table if not exists users(
-        user_id integer primary key autoincrement,
-        name text,
-        username text,
-        password text,
-        email text,
-        avatar text
-    );
-
-
-    '''
-
-    sql.execute(query)
-
-
-    query = '''
-
-    Create table if not exists chores(
-        Speak tamil for a day text,
-        Fold clothes,
-        Fold clothes,
-        Fold bed,
-        Chess
-
-
-    );
-
-
-    '''
-
-    sql.execute(query)
-    
-
-
-def insert_test_data(name, username, password, email, avatar):
+#CREATE user
+def user_create(name, username, password, email, avatar):
 
     query='''
     INSERT into users (name, username, password, email, avatar) 
@@ -47,9 +11,18 @@ def insert_test_data(name, username, password, email, avatar):
     sql.execute(query, [name, username, password, email, avatar])
     connection.commit()
 
-create_tables()
+#RETRIEVE user
 
-insert_test_data("Skippy","skipster","123","sk@aol.com","http://")
-insert_test_data("Charley","charles","123","ch@aol.com","http://")
-insert_test_data("Princess","pinky","123","pi@aol.com","http://")
-insert_test_data("Elly","ellen","123","el@aol.com","http://")
+
+#UPDATE user
+
+
+#DELETE user
+
+
+#CREATE TEST DATA
+def insert_test_data():
+    user_create("Skippy","skipster","123","sk@aol.com","http://")
+    user_create("Charley","charles","123","ch@aol.com","http://")
+    user_create("Princess","pinky","123","pi@aol.com","http://")
+    user_create("Elly","ellen","123","el@aol.com","http://")
